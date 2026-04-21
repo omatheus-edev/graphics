e3768a626cc525da47e8e96ad86175597744a2fa
omatheus-edev <mc2019750@gmail.com>
2026-04-21 10:20:21 -0300

    adding multiples cubes

--- src/main.cpp
+++ src/main.cpp
@@ -49,16 +49,35 @@ int main(int argc, char* argv[]) {
     glEnable(GL_DEPTH_TEST);
     Cube cube;
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,   0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f,  -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f,  -3.5f),
+        glm::vec3(-1.7f,  3.0f,  -7.5f),
+        glm::vec3( 1.3f, -2.0f,  -2.5f),
+        glm::vec3( 1.5f,  2.0f,  -2.5f),
+        glm::vec3( 1.5f,  0.2f,  -1.5f),
+        glm::vec3(-1.3f,  1.0f,  -1.5f)
+    };
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
         Renderer::beginFrame();
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float) glfwGetTime() * 0.8f, glm::vec3(0.0, 1.0, 0.0));
-        model = glm::rotate(model, glm::radians(25.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        for (unsigned int i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
 
-        Renderer::draw(cube, triangleShader, model);
+            float angle = 20.0f * i;
+            if (i % 3 == 0) {
+                angle = (float) glfwGetTime() * 25.0f * (i + 1);
+            }
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 
+            Renderer::draw(cube, triangleShader, model);
+        }
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
